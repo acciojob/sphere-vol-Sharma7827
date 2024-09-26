4f3c1e90995a52fac1484a6688509d92b4efc5bc
Sharma7827 <sharmas4063@gmail.com>
2024-09-26 12:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-let radius = document.querySelector("#radius")
-let volume = document.querySelector("#volume")
-function volume_sphere() {
-    //Write your code here
-	let radius = parseFloat(radius.value);
 
-	if (is(radius) || radius < 0) {
-        volume.innerText = 'NaN' 
-        return
-    }
-	
-	let volumeOfSphere = (4 / 3) * Math.PI * Math.pow(radius, 3)
-
-    volume.innerText = volumeOfSphere.toFixed(4)	
-  
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
